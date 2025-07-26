c631d8c4807435d5ceaab4bfebb74c40b2340552
Ovler <ovlertheyoung@gmail.com>
2025-07-26 23:24:31 +0800

    Revert "chore: no .clone for uri_mut"
    This reverts commit 737eabada829b06e235ee902fb2174896859a1c2.

--- src/proxy.rs
+++ src/proxy.rs
@@ -180,7 +180,7 @@ impl Proxy {
         };
 
         // Update the request URI
-        *req.uri_mut() = new_uri;
+        *req.uri_mut() = new_uri.clone();
 
         // Remove proxy-specific headers that shouldn't be forwarded
         req.headers_mut().remove(PROXY_AUTHORIZATION);
